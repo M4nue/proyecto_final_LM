dcceadde675a2267e7b548ed6375e62464bf648e
M4nue <mrodcur12042005@gmail.com>
2024-05-13 19:26:19 +0200

    Creacion de ruta /10img que es una ruta hacia la pagina 10img.html que muestra 10 imagenes aleatorias de gatos

--- app.py
+++ app.py
@@ -1,57 +1,66 @@
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, abort, request
 import requests
-from static import funciones
-
-datos_razas=[]
+import json
 
 app = Flask(__name__)
+datos_razas=[]
+
 
 @app.route('/')
-def inicio():
-    codigo = requests.get("https://api.thecatapi.com/v1/images/search?limit=10")
-    contenido=codigo.json()
-    url_imagenes = []
-    for url in contenido:
-        url_imagenes.append(url["url"])
-
-    return render_template("inicio.html",imagenes=url_imagenes)
-
-
-@app.route('/razas')
 def razas():
+    datos_razas.clear()
     razas = requests.get("https://api.thecatapi.com/v1/breeds")
     razas_json= razas.json()
-    datos_razas.append(razas_json)
+
+    for raza in razas_json:
+        datos_razas.append(raza)
+    
     list_raza = []
     for raza in razas_json:
         list_raza.append(raza["name"])
     list_raza.sort()
-    return render_template("razas.html",razas_gato=list_raza)
+    
+    return render_template("inicio.html",razas_gato=list_raza)
 
+
+@app.route('/10img')
+def inicio():
+    codigo = requests.get("https://api.thecatapi.com/v1/images/search?limit=10")
+    contenido=codigo.json()
+    url_imagenes = []
+
+    for url in contenido:
+        url_imagenes.append(url["url"])
+
+    return render_template("10img.html",imagenes=url_imagenes)
 
 
 @app.route('/buscador')
 def buscador():
-
-    return render_template("buscador.html")
+    raza = request.args.get('raza', '') 
+    valor=print(raza)
+    return render_template("buscador.html",nombre=valor, raza=raza)
 
 @app.route('/lista', methods=["post"])
 def lista():
-    raza = requests.form.get('raza')
+    raza = request.form.get('raza')
     for datos in datos_razas:
         if raza == datos["name"]:
-            return render_template("lista.html", nombre=datos["name"], enlaces_info=datos["vcahospitals_url"], Temperamento=datos["temperament"],
-                                origen=datos["origin"], descripcion=datos["description"], vida=datos["life_span"],relacion_perros=datos["dog_friendly"],
+            enlaces_info = datos["vetstreet_url"] if "vetstreet_url" in datos else "No disponible"
+            return render_template("lista.html",nombre=datos["name"],enlaces_info=enlaces_info,Temperamento=datos["temperament"],
+                                origen=datos["origin"],descripcion=datos["description"],vida=datos["life_span"],relacion_perros=datos["dog_friendly"],
                                 imagen=datos["reference_image_id"])
-            
+    return abort(404)
 
 
+
+'''
 @app.route('/detalle/<nombre>')
 def cajanombre(nombre):
     for caja in archivo_json:
         if caja["nombre"] == nombre:
             return render_template("detalle.html", nombre=nombre)
     return abort(404)
-
+*/'''
 
 app.run("0.0.0.0",5000,debug=True)
