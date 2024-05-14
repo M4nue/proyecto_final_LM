1887285c5fdc77aaa5b1126318a5b419b7e5d93e
M4nue <mrodcur12042005@gmail.com>
2024-05-14 20:11:02 +0200

    modificación para obtener la API_KEY del archivo .env

--- solicitud_clave.py
+++ solicitud_clave.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
 import requests
+
+load_dotenv ()
+API_KEY=os.getenv('KEY')
 
 raza = input("Introduce la raza de gato en ingles: ")
 
-codigo = requests.get("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids="+raza+"&api_key=live_u7HNRzqeBOAtiwurs7IROBki2UYor2vW8bsSGAgJ8kR3QyzRRHTmgzeI6mWtDaCZ", timeout=10)
+codigo = requests.get("https://api.thecatapi.com/v1/images/search?limit=3&breed_ids="+raza+"&api_key="+API_KEY, timeout=10)
 contenido=codigo.json()
 
 print(codigo)
